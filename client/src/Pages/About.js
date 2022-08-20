e66e3d07174b2fb0972848fa2ad6d5b59d27d48f
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-08-19 22:38:08 -0400

    Add files via upload

---
+++ client/src/Pages/About.js
@@ -0,0 +1,16 @@
+import {Text} from 'react';
+
+function About() {
+    return(
+        <> 
+     <div className="container" style={{textAlign: "center", backgroundColor: '#D77AFF'}}>
+        <Text style={{fontSize:50, color:'black', justifyContent:'center', textAlign:'center',letterSpacing: 10,marginTop: 45}}>NoteEx (Note's Exchange) is your marketplace for University Notes from classes ranging in various subjects such Math and Physics. Users can post their notes in pdf format where it can be purchased and downloaded. The site takes one percent of each sale. NB: Users are NOT allowed to sell quizzes/exams answers. Such listings will be removed and the seller will be disciplined according to our guidelines.   </Text>
+     </div>
+     </>
+    )
+  };
+
+  export default About;
+
+
+ 

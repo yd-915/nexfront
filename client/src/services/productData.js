73088dc5129f470c670b1df47c23f5fb27f48056
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-09-03 22:23:49 +0000

    Update productData.js

--- client/src/services/productData.js
+++ client/src/services/productData.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://nexnotesapp.shop/';
+const baseUrl = 'https://nexnotesapp.herokuapp.com/';
 
 export async function getAll(page, category, query) {
     if (query !== "" && query !== undefined) {
